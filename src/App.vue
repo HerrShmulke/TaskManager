026117290c7999bd2656be4b1495b6e2ef43c626
HerrShmulke <30705015+HerrShmulke@users.noreply.github.com>
2020-12-10 01:48:54 +0700

    Dev list

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="list-wrapper"></div>
+  <div class="list-wrapper">
+    <VList header="Список дел" />
+  </div>
 </template>
 
-<script></script>
+<script>
+  import VList from '@/components/VList.vue';
+
+  export default {
+    components: {
+      VList,
+    },
+  };
+</script>
+
+<style scoped>
+  .list-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+  }
+</style>
 
 <style>
   :root {
     --aquaDark: #8cd4cb;
+    --yellowLight: #f9f3e5;
+    --black: #33322e;
   }
 
   body {
     background-color: var(--aquaDark);
+    font-family: 'Roboto', sans-serif;
   }
 </style>
--- src/components/VList.vue
+++ src/components/VList.vue
@@ -1,1 +1,39 @@
-<template> </template>
+<template>
+  <div class="list">
+    <div class="list__header">
+      <h2 class="list__header-text">{{ header }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      header: String,
+    },
+  };
+</script>
+
+<style scoped>
+  .list {
+    border-radius: 24px;
+    border: solid 4px var(--black);
+    background-color: var(--yellowLight);
+    box-shadow: 12px 12px 0px var(--black);
+    width: 280px;
+    min-height: 300px;
+  }
+
+  .list__header {
+    border-bottom: 4px solid var(--black);
+  }
+
+  .list__header-text {
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 120%;
+    color: var(--black);
+    margin: 0 0 0 25px;
+    padding: 16px 0 10px;
+  }
+</style>
